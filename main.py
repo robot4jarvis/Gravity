6b330b5ba3f830e1dbcad138615aa45efb47755c
robot4jarvis <2010jarvis@gmail.com>
2021-03-31 23:32:44 +0200

    NOVA

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ G = 6.67408e-11  # Universal gravitational constant (6.67408e-11)
 def setBodies():
     """Takes all info from file 'bodies.txt' and pastes it to object list 'bodyList[]'."""
     with open("bodies.txt") as file:  # open the file with the list of bodies
-        for line in "bodies.txt":   # read each line
+        for line in "bodies.txt":  # read each line
             reading = file.readline()  # read a line
-            if reading[0] == "#" or reading[0] == "\n":   # pass a line if it's a comment or if it's in blank
+            if reading[0] == "#" or reading[0] == "\n":  # pass a line if it's a comment or if it's in blank
                 pass
             else:
-                if reading[0] == "/":   # end file if a certain character is found
+                if reading[0] == "/":  # end file if a certain character is found
                     break
-                lect = reading.split(";")    # split line in parts
+                lect = reading.split(";")  # split line in parts
                 bodyList.append(Body(lect[0], lect[1], lect[2], lect[3], lect[4], lect[5]))  # adds an object to "bodyList" that contains the information needed.
 
 
@@ -60,8 +60,9 @@ def gravityAxis(A, B, axis):
     """A:Body where the force is being applied, B:body that applies the force, 'axis': axis nº."""
     dAxis = float(A.pos[axis]) - float(B.pos[axis])  # Determines the distance in axis "axis"
     pytDist = hypot((float(A.pos[0]) - float(B.pos[0])), (float(A.pos[1]) - float(B.pos[1])), (float(A.pos[2]) - float(B.pos[2])))  # determines the pythagorean distance
-    gravity = (A.m * B.m * G)/(pytDist**2)  # determines the module of gravity
-    return (gravity*dAxis)/pytDist  # returns the gravity in a certain axis
+    gravity = (A.m * B.m * G) / (pytDist ** 2)  # determines the module of gravity
+    return (gravity * dAxis) / pytDist  # returns the gravity in a certain axis
+
 
 setBodies()  # creates an object for every body in the system (file bodies.txt)
 # """
