d4f8ffb59930fc688a1e39dd9214aecb356af502
robot4jarvis <2010jarvis@gmail.com>
2021-04-01 12:15:30 +0200

    Created module functions.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from math import hypot
-
-from functions import addGravities, setBodies, Body, gravityAxis, bodyList
+from functions import addGravities, setBodies, bodyList
 
 setBodies()  # creates an object for every body in the system (file bodies.txt)
 for i in bodyList:  # computates the gravities for all bodies
